e5706ea3edec00fb2180a3acaae568e0e1e7d60f
Random Coder <vinibritocoder@gmail.com>
2020-11-05 15:38:41 -0300

    Protótipo inicial

---
+++ src/header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+      <marquee behavior="scroll" direction="left">
+      <h2>Bem vindo (a) ao processador de informações. Feito por: Vinícius Brito Bastos Antunes (COMP-22)</h2>
+      </marquee>
+  );
+};
+
+export default Header;
